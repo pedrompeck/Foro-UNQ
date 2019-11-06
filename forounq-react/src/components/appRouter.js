e610658f2ed4212f05c7e1a767c5160b5e6e69ac
pedro <ppedro@fusap.com.ar>
2019-11-06 20:13:48 -0300

    Se agrega ruta de login a la interfaz

--- forounq-react/src/components/appRouter.js
+++ forounq-react/src/components/appRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Materias from './materias';
+import Login from './login';
 import Home from './home';
 
 class AppRouter extends React.Component {
@@ -10,6 +11,7 @@ class AppRouter extends React.Component {
         return (
           <BrowserRouter>
                 <Route exact path="/materias" component={Materias} />
+                <Route exact path="/" component={Login} />
           </BrowserRouter>
         )
     }
---
+++ forounq-react/src/components/login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { materias } from '../api/api';
+
+class Login extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+           userName: '',
+           password: '',
+        }
+    }
+
+    componentDidMount() {
+        materias()
+        .then(response => {
+          console.log(response);
+          this.setState({
+                materias: response
+            })  
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+   
+    render() {
+        return(
+            <div className="container">
+                        <h1> FORO </h1>
+                            <div>
+                              <p>Login</p>
+                              <p>Olvide contraseña</p>
+                             </div>
+                    </div>
+        );
+    }
+}
+
+export default Login;
